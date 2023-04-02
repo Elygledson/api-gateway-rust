7ee0f379c9a3b5eb1381869ba9f1a609082f3728
JhonathanMilk <johnathanleitealves4@gmail.com>
2023-04-02 12:21:38 -0300

    alterado alguns tratamentos de erro

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use serde_json::Value;
 use std::collections::HashSet;
 use rocket_contrib::json::{Json, JsonValue};
 use serde::{Serialize, Deserialize};
+use reqwest::StatusCode;
 
 #[macro_use] extern crate rocket;
 
@@ -20,16 +21,41 @@ fn hello(name: String, age: u8) -> String {
 }
 
 #[get("/", rank = 2)]
-fn index_get() -> &'static str {
-    "This is the API gateway, please use POST method to call the microservice"
+fn index_get() -> Result<Json<ResponseData>, String> {
+    let client = reqwest::blocking::Client::new();
+    let response_result = client.get("http://viacep.com.br/ws/010")
+        .send();
+    match response_result {
+        Ok(response) => {
+            match response.status() {
+                StatusCode::OK => {
+                    let body_result = response.text();
+                    match body_result {
+                        Ok(body) => {
+                            let response_data = ResponseData { message: body };
+                            Ok(Json(response_data))
+                        }
+                        Err(e) => Err(format!("Failed to get response body: {}", e))
+                    }
+                },
+                StatusCode::NOT_FOUND => {
+                    Err("URL não encontrada".to_string())
+                },
+                _ => {
+                    Err(format!("Erro ao fazer a requisição: {}", response.status()))
+                }
+            }
+        },
+        Err(e) => Err(format!("Failed to send request: {}", e))
+    }
 }
 
 
 
 #[derive(Serialize, Deserialize)]
-struct RequestData {
-    name: String,
-    age: u8,
+struct RequestBody {
+    service_type: String,
+    data: Value,
 }
 
 #[derive(Serialize)]
@@ -38,15 +64,24 @@ struct ResponseData {
 }
 
 #[post("/", format = "json", data = "<data>")]
-fn index_post(data: Json<RequestData>) -> Json<ResponseData> {
+fn index_post(data: Json<RequestBody>) -> Json<Result<ResponseData, String>> {
     let client = reqwest::blocking::Client::new();
-    let response = client.post("http://localhost:8083")
+    let response_result = client.post("http://localhost:8083")
         .json(&data.0)
-        .send()
-        .unwrap();
-    let body = response.text().unwrap();
-    let response_data = ResponseData { message: body };
-    return Json(response_data);
+        .send();
+    match response_result {
+        Ok(response) => {
+            let body_result = response.text();
+            match body_result {
+                Ok(body) => {
+                    let response_data = ResponseData { message: body };
+                    Json(Ok(response_data))
+                }
+                Err(e) => Json(Err(format!("Failed to get response body: {}", e)))
+            }
+        }
+        Err(e) => Json(Err(format!("Failed to send request: {}", e)))
+    }
 }
 
 
